33448753bbbc9beb65276252a36c54a2e6c99f02
Doug Davis <ddavis@ddavis.io>
2019-02-07 14:38:47 -0500

    versioning work

---
+++ .ci-scripts/srht-pypi.py
@@ -0,0 +1,19 @@
+import subprocess
+import pygram11.version
+
+def main():
+    res = subprocess.run(['git', 'describe'], stdout=subprocess.PIPE)
+    describe_out = res.stdout.decode('utf-8').split('-')
+    print(describe_out)
+    if len(describe_out) > 1:
+        return 0
+    elif pygram11.version.version == describe_out[0].strip():
+        #res = subprocess.run('twine upload dist/*', shell=True)
+        #return res.returncode
+        print('****** would be uploading')
+        return 0
+    else:
+        return 0;
+
+if __name__ == '__main__':
+    main()
