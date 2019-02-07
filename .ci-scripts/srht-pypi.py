580aab425452594d098df09c01b37358a2479d45
Doug Davis <ddavis@ddavis.io>
2019-02-07 15:27:06 -0500

    make srht deploy to pypi

--- .ci-scripts/srht-pypi.py
+++ .ci-scripts/srht-pypi.py
@@ -1,5 +1,6 @@
 import subprocess
 import pygram11.version
+import sys
 
 def main():
     res = subprocess.run(['git', 'describe'], stdout=subprocess.PIPE)
@@ -8,12 +9,11 @@ def main():
     if len(describe_out) > 1:
         return 0
     elif pygram11.version.version == describe_out[0].strip():
-        #res = subprocess.run('twine upload dist/*', shell=True)
-        #return res.returncode
-        print('****** would be uploading')
-        return 0
+        res = subprocess.run('twine upload dist/*', shell=True)
+        return res.returncode
     else:
         return 0;
 
 if __name__ == '__main__':
     main()
+    sys.exit()
