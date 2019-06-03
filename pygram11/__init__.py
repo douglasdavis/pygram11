98aeef47d19309a30d94a2a72997d47463c616ab
Doug Davis <ddavis@ddavis.io>
2019-06-02 20:26:50 -0400

    version bump

--- pygram11/__init__.py
+++ pygram11/__init__.py
@@ -2,7 +2,7 @@ from .hist import fix1d, var1d, fix2d, var2d
 from .hist import histogram, histogram2d
 from ._core import _HAS_OPENMP
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 __all__ = ["fix1d", "var1d", "fix2d", "var2d", "histogram", "histogram2d"]
 
