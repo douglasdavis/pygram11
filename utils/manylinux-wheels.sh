141da65036afc97fbbb82a38ec16652b510d48d9
Doug Davis <ddavis@ddavis.io>
2020-04-26 18:27:42 -0400

    improve manylinux script

--- utils/manylinux-wheels.sh
+++ utils/manylinux-wheels.sh
@@ -2,7 +2,10 @@
 
 ## This script will build manylinux2010 x86_64 wheels; it should be
 ## executed from the root project directory while inside a
-## manylinux{2010,2014} docker container
+## manylinux{1,2010} docker container
+## Example shell command:
+## $ docker run --rm -it -w $PWD -v $PWD:$PWD quay.io/pypa/manylinux2010_x86_64 \
+##     /bin/bash utils/manylinux-wheels.sh
 
 set -e -x
 
