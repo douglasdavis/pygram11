206de34581ad523a3a1f8eb1e45ea4d111cee3d0
Doug Davis <ddavis@ddavis.io>
2020-10-03 11:22:47 -0400

    version

--- src/pygram11/__init__.py
+++ src/pygram11/__init__.py
@@ -32,7 +32,7 @@ from .hist import fix1d, fix1dmw, var1d, var1dmw
 from .hist import fix2d, var2d
 from .hist import histogram, histogram2d
 
-__version__ = "0.10.2.dev0"
+__version__ = "0.10.2"
 version_info = tuple(__version__.split("."))
 
 
