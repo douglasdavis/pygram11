cc009db16263a7c9c5ac68ac44d52ddb23d1d2aa
Doug Davis <ddavis@ddavis.io>
2020-10-05 13:07:04 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
-    "pallets_sphinx_themes",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
