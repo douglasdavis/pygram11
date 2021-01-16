263ab7eab615d053fcd5c509a342cc6115749ac4
Doug Davis <ddavis@ddavis.io>
2021-01-16 15:27:15 -0500

    adding optimized unweighted histogram calculations

--- src/_backend1d.cpp
+++ src/_backend1d.cpp
@@ -28,11 +28,56 @@
 
 namespace py = pybind11;
 
+template <typename T>
+py::array_t<py::ssize_t> f1d(
+    const py::array_t<T, py::array::c_style | py::array::forcecast>& x, py::ssize_t nbins,
+    double xmin, double xmax, bool flow) {
+  py::ssize_t nx = x.shape(0);
+  double norm = nbins / (xmax - xmin);
+  py::array_t<py::ssize_t> counts(nbins);
+  std::memset(counts.mutable_data(), 0, sizeof(py::ssize_t) * nbins);
+  py::ssize_t* counts_proxy = counts.mutable_unchecked().mutable_data();
+  const T* x_proxy = x.unchecked().data();
+
+  if (nx > 50000) {
+    if (flow) {
+      pygram11::helpers::fill_parallel_flow(x_proxy, nx, nbins, xmin, xmax, norm,
+                                            counts_proxy);
+    }
+    else {
+      pygram11::helpers::fill_parallel_noflow(x_proxy, nx, nbins, xmin, xmax, norm,
+                                              counts_proxy);
+    }
+  }
+
+  else {
+    if (flow) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
+        auto bin = pygram11::helpers::get_bin(x_proxy[i], nbins, xmin, xmax, norm);
+        counts_proxy[bin]++;
+      }
+    }
+    else {
+      for (py::ssize_t i = 0; i < nx; ++i) {
+        if (x_proxy[i] < xmin || x_proxy[i] >= xmax) {
+          continue;
+        }
+        else {
+          auto bin = pygram11::helpers::get_bin(x_proxy[i], xmin, norm);
+          counts_proxy[bin]++;
+        }
+      }
+    }
+  }
+
+  return counts;
+}
+
 template <typename T1, typename T2>
 py::tuple f1dw(const py::array_t<T1, py::array::c_style | py::array::forcecast>& x,
                const py::array_t<T2, py::array::c_style | py::array::forcecast>& w,
                py::ssize_t nbins, T1 xmin, T1 xmax, bool flow, bool density, bool as_err) {
-  py::ssize_t ndata = x.shape(0);
+  py::ssize_t nx = x.shape(0);
   T1 norm = nbins / (xmax - xmin);
   py::array_t<T2> counts({nbins});
   py::array_t<T2> vars({nbins});
@@ -43,27 +88,27 @@ py::tuple f1dw(const py::array_t<T1, py::array::c_style | py::array::forcecast>&
   const T1* x_proxy = x.data();
   const T2* w_proxy = w.data();
 
-  if (ndata > 5000) {
+  if (nx > 5000) {
     if (flow) {
-      pygram11::helpers::fill_parallel_flow(x_proxy, w_proxy, ndata, nbins, xmin, xmax,
+      pygram11::helpers::fill_parallel_flow(x_proxy, w_proxy, nx, nbins, xmin, xmax,
                                             norm, counts_proxy, vars_proxy);
     }
     else {
-      pygram11::helpers::fill_parallel_noflow(x_proxy, w_proxy, ndata, nbins, xmin, xmax,
+      pygram11::helpers::fill_parallel_noflow(x_proxy, w_proxy, nx, nbins, xmin, xmax,
                                               norm, counts_proxy, vars_proxy);
     }
   }
 
   else {
     if (flow) {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         auto bin = pygram11::helpers::get_bin(x_proxy[i], nbins, xmin, xmax, norm);
         counts_proxy[bin] += w_proxy[i];
         vars_proxy[bin] += w_proxy[i] * w_proxy[i];
       }
     }
     else {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         if (x_proxy[i] < xmin || x_proxy[i] >= xmax) {
           continue;
         }
@@ -92,7 +137,7 @@ py::tuple v1dw(const py::array_t<T1, py::array::c_style | py::array::forcecast>&
                const py::array_t<T2, py::array::c_style | py::array::forcecast>& w,
                const py::array_t<T1, py::array::c_style | py::array::forcecast>& edges,
                bool flow, bool density, bool as_err) {
-  py::ssize_t ndata = x.shape(0);
+  py::ssize_t nx = x.shape(0);
   py::ssize_t nedges = edges.shape(0);
   py::ssize_t nbins = nedges - 1;
   std::vector<T1> edges_v(nedges);
@@ -107,13 +152,13 @@ py::tuple v1dw(const py::array_t<T1, py::array::c_style | py::array::forcecast>&
   const T1* x_proxy = x.data();
   const T2* w_proxy = w.data();
 
-  if (ndata > 5000) {
+  if (nx > 5000) {
     if (flow) {
-      pygram11::helpers::fill_parallel_flow(x_proxy, w_proxy, edges_v, ndata, counts_proxy,
+      pygram11::helpers::fill_parallel_flow(x_proxy, w_proxy, edges_v, nx, counts_proxy,
                                             vars_proxy);
     }
     else {
-      pygram11::helpers::fill_parallel_noflow(x_proxy, w_proxy, edges_v, ndata,
+      pygram11::helpers::fill_parallel_noflow(x_proxy, w_proxy, edges_v, nx,
                                               counts_proxy, vars_proxy);
     }
   }
@@ -121,14 +166,14 @@ py::tuple v1dw(const py::array_t<T1, py::array::c_style | py::array::forcecast>&
     T1 xmin = edges_v.front();
     T1 xmax = edges_v.back();
     if (flow) {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         auto bin = pygram11::helpers::get_bin(x_proxy[i], nbins, edges_v);
         counts_proxy[bin] += w_proxy[i];
         vars_proxy[bin] += w_proxy[i] * w_proxy[i];
       }
     }
     else {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         if (x_proxy[i] < xmin || x_proxy[i] >= xmax) {
           continue;
         }
@@ -156,7 +201,7 @@ template <typename T1, typename T2>
 py::tuple f1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>& x,
                 const py::array_t<T2, py::array::c_style | py::array::forcecast>& w,
                 py::ssize_t nbins, T1 xmin, T1 xmax, bool flow, bool as_err) {
-  py::ssize_t ndata = x.shape(0);
+  py::ssize_t nx = x.shape(0);
   py::ssize_t nweightvars = w.shape(1);
   py::array_t<T2> counts({nbins, nweightvars});
   py::array_t<T2> vars({nbins, nweightvars});
@@ -164,7 +209,7 @@ py::tuple f1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
   std::memset(vars.mutable_data(), 0, sizeof(T2) * nbins * nweightvars);
 
   // go to parallel fillers for large datasets
-  if (ndata > 5000) {
+  if (nx > 5000) {
     if (flow) {
       pygram11::helpers::fillmw_parallel_flow(x, w, nbins, xmin, xmax, counts, vars);
     }
@@ -181,7 +226,7 @@ py::tuple f1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
     auto w_proxy = w.template unchecked<2>();
     T1 norm = nbins / (xmax - xmin);
     if (flow) {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         T1 x_i = x_proxy(i);
         auto bin = pygram11::helpers::get_bin(x_i, nbins, xmin, xmax, norm);
         for (py::ssize_t j = 0; j < nweightvars; ++j) {
@@ -192,7 +237,7 @@ py::tuple f1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
       }
     }
     else {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         T1 x_i = x_proxy(i);
         if (x_i < xmin || x_i >= xmax) {
           continue;
@@ -230,8 +275,8 @@ py::tuple v1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
   std::memset(counts.mutable_data(), 0, sizeof(T2) * nbins * nweightvars);
   std::memset(vars.mutable_data(), 0, sizeof(T2) * nbins * nweightvars);
 
-  py::ssize_t ndata = x.shape(0);
-  if (ndata > 5000) {
+  py::ssize_t nx = x.shape(0);
+  if (nx > 5000) {
     if (flow) {
       pygram11::helpers::fillmw_parallel_flow(x, w, edges_v, counts, vars);
     }
@@ -247,7 +292,7 @@ py::tuple v1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
     T1 xmin = edges_v.front();
     T2 xmax = edges_v.back();
     if (flow) {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         auto bin = pygram11::helpers::get_bin(x_proxy(i), nbins, edges_v);
         for (py::ssize_t j = 0; j < nweightvars; ++j) {
           T2 w_ij = w_proxy(i, j);
@@ -257,7 +302,7 @@ py::tuple v1dmw(const py::array_t<T1, py::array::c_style | py::array::forcecast>
       }
     }
     else {
-      for (py::ssize_t i = 0; i < ndata; ++i) {
+      for (py::ssize_t i = 0; i < nx; ++i) {
         T1 x_i = x_proxy(i);
         if (x_i < xmin || x_i >= xmax) {
           continue;
@@ -283,6 +328,10 @@ PYBIND11_MODULE(_backend1d, m) {
   m.doc() = "pygram11's pybind11 based 1D backend";
 
   m.def("_omp_get_max_threads", []() { return omp_get_max_threads(); });
+
+  m.def("_f1d", &f1d<double>);
+  m.def("_f1d", &f1d<float>);
+  m.def("_f1d", &f1d<py::ssize_t>);
 
   m.def("_f1dw", &f1dw<double, double>);
   m.def("_f1dw", &f1dw<float, float>);
