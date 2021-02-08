33e6ed76b977c313eb74a1b8e5c7ae2c0ac01dc2
Doug Davis <ddavis@ddavis.io>
2021-02-08 13:25:58 -0500

    tests

--- tests/test_hist.py
+++ tests/test_hist.py
@@ -32,19 +32,6 @@ import pytest
 
 rng = np.random.default_rng()
 
-DATA_FILE = Path(__file__).parent / "data" / "test_data.npz"
-DATA_RAW = np.load(DATA_FILE)
-X_RAWF = DATA_RAW["data_f64"]
-X_RAWI = DATA_RAW["data_ui32"]
-W_RAW = DATA_RAW["weights_f32"]
-# fmt: off
-E_ARRS = (
-    np.array([27.5, 35.1, 40.2, 50.1, 57.2, 60.1,
-              64.2, 70.2, 90.1, 98.2, 110.1, 120.2,
-              130.2, 160.1, 200.2, 250.1]),
-    np.array([1.1, 17.2, 32.9, 100.2,
-              170.5, 172.1, 173.1, 279.2]),
-)
 XTYPES = (np.float32, np.float64, np.int32, np.int64, np.uint32, np.uint64)
 Psize = struct.calcsize("P") * 8
 if Psize == 32:
@@ -52,29 +39,23 @@ if Psize == 32:
 # fmt: on
 
 
-def make_data_1d(xtype, wtype=None):
-    x = X_RAWF.astype(dtype=xtype) * rng.uniform(0.8, 1.2, X_RAWF.shape[0])
-    w = None if wtype is None else rng.uniform(0.7, 1.8, x.shape[0])
-    return x, w
-
-
-def make_data_1d_mw(xtype, wtype):
-    x = X_RAWF.astype(dtype=xtype)
-    w1 = W_RAW.astype(dtype=wtype) * 0.1
-    w2 = W_RAW.astype(dtype=wtype) * 1.1
-    w3 = W_RAW.astype(dtype=wtype) * rng.uniform(-0.3, 4.1, w1.shape[0])
-    w = np.transpose(np.array([w1, w2, w3]))
-    return x, w
-
-
-def make_data_2d(xtype, ytype, wtype=None):
-    x = X_RAWF.astype(dtype=xtype) * rng.uniform(0.8, 1.2, X_RAWF.shape[0])
-    y = (X_RAWF * rng.uniform(0.8, 1.2, x.shape[0])).astype(ytype)
-    w = None if wtype is None else rng.uniform(0.75, 1.25, x.shape[0])
-    return x, y, w
-
-
 class TestFix1D:
+    XD = rng.standard_normal(10000)
+    WD = rng.uniform(0.8, 1.2, XD.shape[0])
+
+    def make_data(self, xtype, wtype):
+        x = (self.XD * rng.uniform(0.9, 1.1, self.XD.shape[0])).astype(xtype)
+        w = (self.WD * rng.uniform(0.9, 1.1, self.XD.shape[0])).astype(wtype)
+        return x, w
+
+    def make_data_mw(self, xtype, wtype):
+        x = self.XD.astype(dtype=xtype)
+        w1 = (self.WD * 0.1).astype(wtype)
+        w2 = (self.WD * 1.1).astype(wtype)
+        w3 = (self.WD * rng.uniform(-0.3, 4.1, w1.shape[0])).astype(wtype)
+        w = np.transpose(np.array([w1, w2, w3], dtype=wtype))
+        return x, w
+
     @pytest.mark.parametrize("xtype", XTYPES)
     @pytest.mark.parametrize("wtype", [None, np.float32, np.float64])
     @pytest.mark.parametrize("density", [True, False])
@@ -87,8 +68,8 @@ class TestFix1D:
         if density and flow:
             assert True
             return
-        x, w = make_data_1d(xtype, wtype)
-        n, xmin, xmax = 25, 40.5, 180.5
+        x, w = self.make_data(xtype, wtype)
+        n, xmin, xmax = 25, -3.1, 3.1
         res0, err0 = func(
             x, weights=w, bins=n, range=(xmin, xmax), density=density, flow=flow
         )
@@ -112,8 +93,8 @@ class TestFix1D:
     @pytest.mark.OD
     def test_multiple_weights(self, xtype, wtype, flow, ompt, func):
         pg.FIXED_WIDTH_MW_PARALLEL_THRESHOLD = ompt
-        x, w = make_data_1d_mw(xtype, wtype)
-        n, xmin, xmax = 35, 40.4, 190.2
+        x, w = self.make_data_mw(xtype, wtype)
+        n, xmin, xmax = 25, -3.1, 3.1
         res0, err0 = func(x, weights=w, bins=n, range=(xmin, xmax), flow=flow)
         for i in range(res0.shape[1]):
             res1, edge = np.histogram(x, weights=w[:, i], bins=n, range=(xmin, xmax))
@@ -123,53 +104,53 @@ class TestFix1D:
             npt.assert_allclose(res0[:, i], res1, rtol=1e-05, atol=1e-08)
 
 
-class TestVar1D:
-    @pytest.mark.parametrize("xtype", XTYPES)
-    @pytest.mark.parametrize("wtype", [None, np.float32, np.float64])
-    @pytest.mark.parametrize("bins", E_ARRS)
-    @pytest.mark.parametrize("density", [True, False])
-    @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
-    @pytest.mark.parametrize("func", [pg.histogram, pg.var1d])
-    @pytest.mark.OD
-    def test_no_weight_and_single_weight(
-        self, xtype, wtype, bins, density, flow, ompt, func
-    ):
-        pg.VARIABLE_WIDTH_PARALLEL_THRESHOLD = ompt
-        if density and flow:
-            assert True
-            return
-        x, w = make_data_1d(xtype, wtype)
-        res0, err0 = func(x, weights=w, bins=bins, density=density, flow=flow)
-        res1, edge = np.histogram(x, weights=w, bins=bins, density=density)
-        xmin, xmax = bins[0], bins[-1]
-        if flow:
-            if w is None:
-                res1[0] += np.sum(x < xmin)
-                res1[-1] += np.sum(x >= xmax)
-            else:
-                res1[0] += w[x < xmin].sum()
-                res1[-1] += w[x >= xmax].sum()
-        npt.assert_allclose(res0, res1, rtol=1e-05, atol=1e-08)
+# class TestVar1D:
+#     @pytest.mark.parametrize("xtype", XTYPES)
+#     @pytest.mark.parametrize("wtype", [None, np.float32, np.float64])
+#     @pytest.mark.parametrize("bins", E_ARRS)
+#     @pytest.mark.parametrize("density", [True, False])
+#     @pytest.mark.parametrize("flow", [True, False])
+#     @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
+#     @pytest.mark.parametrize("func", [pg.histogram, pg.var1d])
+#     @pytest.mark.OD
+#     def test_no_weight_and_single_weight(
+#         self, xtype, wtype, bins, density, flow, ompt, func
+#     ):
+#         pg.VARIABLE_WIDTH_PARALLEL_THRESHOLD = ompt
+#         if density and flow:
+#             assert True
+#             return
+#         x, w = make_data_1d(xtype, wtype)
+#         res0, err0 = func(x, weights=w, bins=bins, density=density, flow=flow)
+#         res1, edge = np.histogram(x, weights=w, bins=bins, density=density)
+#         xmin, xmax = bins[0], bins[-1]
+#         if flow:
+#             if w is None:
+#                 res1[0] += np.sum(x < xmin)
+#                 res1[-1] += np.sum(x >= xmax)
+#             else:
+#                 res1[0] += w[x < xmin].sum()
+#                 res1[-1] += w[x >= xmax].sum()
+#         npt.assert_allclose(res0, res1, rtol=1e-05, atol=1e-08)
 
-    @pytest.mark.parametrize("xtype", XTYPES)
-    @pytest.mark.parametrize("wtype", [np.float32, np.float64])
-    @pytest.mark.parametrize("bins", E_ARRS)
-    @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
-    @pytest.mark.parametrize("func", [pg.var1dmw, pg.histogram])
-    @pytest.mark.OD
-    def test_multiple_weights(self, xtype, wtype, bins, flow, ompt, func):
-        pg.VARIABLE_WIDTH_MW_PARALLEL_THRESHOLD = ompt
-        x, w = make_data_1d_mw(xtype, wtype)
-        xmin, xmax = bins[0], bins[-1]
-        res0, err0 = func(x, weights=w, bins=bins, flow=flow)
-        for i in range(res0.shape[1]):
-            res1, edge = np.histogram(x, weights=w[:, i], bins=bins)
-            if flow:
-                res1[0] += np.sum(w[:, i][x < xmin])
-                res1[-1] += np.sum(w[:, i][x >= xmax])
-            npt.assert_allclose(res0[:, i], res1, rtol=1e-05, atol=1e-08)
+#     @pytest.mark.parametrize("xtype", XTYPES)
+#     @pytest.mark.parametrize("wtype", [np.float32, np.float64])
+#     @pytest.mark.parametrize("bins", E_ARRS)
+#     @pytest.mark.parametrize("flow", [True, False])
+#     @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
+#     @pytest.mark.parametrize("func", [pg.var1dmw, pg.histogram])
+#     @pytest.mark.OD
+#     def test_multiple_weights(self, xtype, wtype, bins, flow, ompt, func):
+#         pg.VARIABLE_WIDTH_MW_PARALLEL_THRESHOLD = ompt
+#         x, w = make_data_1d_mw(xtype, wtype)
+#         xmin, xmax = bins[0], bins[-1]
+#         res0, err0 = func(x, weights=w, bins=bins, flow=flow)
+#         for i in range(res0.shape[1]):
+#             res1, edge = np.histogram(x, weights=w[:, i], bins=bins)
+#             if flow:
+#                 res1[0] += np.sum(w[:, i][x < xmin])
+#                 res1[-1] += np.sum(w[:, i][x >= xmax])
+#             npt.assert_allclose(res0[:, i], res1, rtol=1e-05, atol=1e-08)
 
 
 class TestFix2D:
@@ -193,8 +174,8 @@ class TestFix2D:
     def test_no_weight_and_single_weight(self, xtype, ytype, wtype, flow, ompt, func):
         pg.FIXED_WIDTH_PARALLEL_THRESHOLD = ompt
         x, y, w = self.make_data(xtype, ytype, wtype)
-        nbx, xmin, xmax = 25, 40.5, 180.5
-        nby, ymin, ymax = 21, 33.3, 178.2
+        nbx, xmin, xmax = 25, -3.1, 3.1
+        nby, ymin, ymax = 15, -3.1, 3.1
         res0, err0 = func(
             x,
             y,
