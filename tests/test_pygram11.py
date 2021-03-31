aeae028d329022bd473af2bda5e07806df185f52
Doug Davis <ddavis@anaconda.com>
2021-03-31 11:43:26 -0400

    tests

--- tests/test_pygram11.py
+++ tests/test_pygram11.py
@@ -23,7 +23,6 @@
 import os
 import multiprocessing
 import struct
-import sys
 from typing import Sequence, Any
 
 import numpy as np
@@ -32,7 +31,14 @@ import pygram11 as pg
 import pytest
 
 RNG = np.random.default_rng(1234)
-XTYPES: Sequence[Any] = (np.float32, np.float64, np.int32, np.int64, np.uint32, np.uint64)
+XTYPES: Sequence[Any] = (
+    np.float32,
+    np.float64,
+    np.int32,
+    np.int64,
+    np.uint32,
+    np.uint64,
+)
 if (struct.calcsize("P") * 8) == 32:
     XTYPES = (np.float32, np.int32, np.uint32)
 
@@ -58,19 +64,17 @@ class TestFix1D:
     @pytest.mark.parametrize("wtype", [None, np.float32, np.float64])
     @pytest.mark.parametrize("density", [True, False])
     @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.histogram, pg.fix1d])
+    @pytest.mark.parametrize("cons_var", [True, False])
     @pytest.mark.OD
-    def test_no_weight_and_single_weight(self, xtype, wtype, density, flow, ompt, func):
-        pg.FIXED_WIDTH_PARALLEL_THRESHOLD_1D = ompt
+    def test_no_weight_and_single_weight(
+        self, xtype, wtype, density, flow, func, cons_var
+    ):
         if density and flow:
             assert True
             return
         x, w = self.make_data(xtype, wtype)
         n, xmin, xmax = 50, -10.1, 10.1
-        res0, err0 = func(
-            x, weights=w, bins=n, range=(xmin, xmax), density=density, flow=flow
-        )
         res1, edge = np.histogram(
             x, weights=w, bins=n, range=(xmin, xmax), density=density
         )
@@ -81,25 +85,78 @@ class TestFix1D:
             else:
                 res1[0] += w[x < xmin].sum()
                 res1[-1] += w[x >= xmax].sum()
-        npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+
+        with pg.omp_disabled():
+            res0, err0 = func(
+                x,
+                weights=w,
+                bins=n,
+                range=(xmin, xmax),
+                density=density,
+                flow=flow,
+                cons_var=cons_var,
+            )
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_forced():
+            res0, err0 = func(
+                x,
+                weights=w,
+                bins=n,
+                range=(xmin, xmax),
+                density=density,
+                flow=flow,
+                cons_var=cons_var,
+            )
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
 
     @pytest.mark.parametrize("xtype", XTYPES)
     @pytest.mark.parametrize("wtype", [np.float32, np.float64])
     @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.histogram, pg.fix1dmw])
     @pytest.mark.OD
-    def test_multiple_weights(self, xtype, wtype, flow, ompt, func):
-        pg.FIXED_WIDTH_MW_PARALLEL_THRESHOLD_1D = ompt
+    def test_multiple_weights(self, xtype, wtype, flow, func):
         x, w = self.make_data_mw(xtype, wtype)
         n, xmin, xmax = 50, -10.1, 10.1
-        res0, err0 = func(x, weights=w, bins=n, range=(xmin, xmax), flow=flow)
-        for i in range(res0.shape[1]):
-            res1, edge = np.histogram(x, weights=w[:, i], bins=n, range=(xmin, xmax))
-            if flow:
-                res1[0] += np.sum(w[:, i][x < xmin])
-                res1[-1] += np.sum(w[:, i][x >= xmax])
-            npt.assert_allclose(res0[:, i], res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_disabled():
+            res0a, err0a = func(x, weights=w, bins=n, range=(xmin, xmax), flow=flow)
+            for i in range(res0a.shape[1]):
+                res1, edge = np.histogram(
+                    x, weights=w[:, i], bins=n, range=(xmin, xmax)
+                )
+                if flow:
+                    res1[0] += np.sum(w[:, i][x < xmin])
+                    res1[-1] += np.sum(w[:, i][x >= xmax])
+                npt.assert_allclose(res0a[:, i], res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_forced():
+            res0a, err0a = func(x, weights=w, bins=n, range=(xmin, xmax), flow=flow)
+            for i in range(res0a.shape[1]):
+                res1, edge = np.histogram(
+                    x, weights=w[:, i], bins=n, range=(xmin, xmax)
+                )
+                if flow:
+                    res1[0] += np.sum(w[:, i][x < xmin])
+                    res1[-1] += np.sum(w[:, i][x >= xmax])
+                npt.assert_allclose(res0a[:, i], res1, atol=0.01, rtol=1.0e-3)
+
+    @pytest.mark.OD
+    @pg.without_omp
+    def test_cons_var_nomp(self):
+        x = RNG.standard_normal(size=(500,))
+        w = 0.5 * np.ones_like(x, dtype=np.float64)
+        res1, var1 = pg.histogram(x, bins=12, range=(-3, 3), weights=w, cons_var=True)
+        res2, edg2 = np.histogram(x, bins=12, range=(-3, 3), weights=w)
+        npt.assert_allclose(res1, res2)
+        npt.assert_allclose(var1, res1 * 0.5)
+
+    @pytest.mark.OD
+    @pg.with_omp
+    def test_cons_var_womp(self):
+        x = RNG.standard_normal(size=(500,))
+        w = 0.5 * np.ones_like(x, dtype=np.float64)
+        res1, var1 = pg.histogram(x, bins=12, range=(-3, 3), weights=w, cons_var=True)
+        res2, edg2 = np.histogram(x, bins=12, range=(-3, 3), weights=w)
+        npt.assert_allclose(res1, res2)
+        npt.assert_allclose(var1, res1 * 0.5)
 
 
 class TestVar1D:
@@ -130,18 +187,13 @@ class TestVar1D:
     @pytest.mark.parametrize("bins", [E1, E2])
     @pytest.mark.parametrize("density", [True, False])
     @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.histogram, pg.var1d])
     @pytest.mark.OD
-    def test_no_weight_and_single_weight(
-        self, xtype, wtype, bins, density, flow, ompt, func
-    ):
-        pg.VARIABLE_WIDTH_PARALLEL_THRESHOLD_1D = ompt
+    def test_no_weight_and_single_weight(self, xtype, wtype, bins, density, flow, func):
         if density and flow:
             assert True
             return
         x, w = self.make_data(xtype, wtype)
-        res0, err0 = func(x, weights=w, bins=bins, density=density, flow=flow)
         res1, edge = np.histogram(x, weights=w, bins=bins, density=density)
         xmin, xmax = bins[0], bins[-1]
         if flow:
@@ -151,17 +203,21 @@ class TestVar1D:
             else:
                 res1[0] += w[x < xmin].sum()
                 res1[-1] += w[x >= xmax].sum()
-        npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+
+        with pg.omp_forced():
+            res0, err0 = func(x, weights=w, bins=bins, density=density, flow=flow)
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_disabled():
+            res0, err0 = func(x, weights=w, bins=bins, density=density, flow=flow)
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
 
     @pytest.mark.parametrize("xtype", XTYPES)
     @pytest.mark.parametrize("wtype", [np.float32, np.float64])
     @pytest.mark.parametrize("bins", [E1, E2])
     @pytest.mark.parametrize("flow", [True, False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.var1dmw, pg.histogram])
     @pytest.mark.OD
-    def test_multiple_weights(self, xtype, wtype, bins, flow, ompt, func):
-        pg.VARIABLE_WIDTH_MW_PARALLEL_THRESHOLD_1D = ompt
+    def test_multiple_weights(self, xtype, wtype, bins, flow, func):
         x, w = self.make_data_mw(xtype, wtype)
         xmin, xmax = bins[0], bins[-1]
         res0, err0 = func(x, weights=w, bins=bins, flow=flow)
@@ -188,27 +244,36 @@ class TestFix2D:
     @pytest.mark.parametrize("ytype", XTYPES)
     @pytest.mark.parametrize("wtype", [None, np.float64, np.float32])
     @pytest.mark.parametrize("flow", [False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.histogram2d, pg.fix2d])
     @pytest.mark.TD
-    def test_no_weight_and_single_weight(self, xtype, ytype, wtype, flow, ompt, func):
-        pg.FIXED_WIDTH_PARALLEL_THRESHOLD_2D = ompt
+    def test_no_weight_and_single_weight(self, xtype, ytype, wtype, flow, func):
         x, y, w = self.make_data(xtype, ytype, wtype)
         nbx, xmin, xmax = 25, -10.1, 10.1
         nby, ymin, ymax = 15, -10.1, 10.1
-        res0, err0 = func(
-            x,
-            y,
-            bins=[nbx, nby],
-            range=((xmin, xmax), (ymin, ymax)),
-            weights=w,
-            flow=flow,
-        )
         res1, ex, ey = np.histogram2d(
             x, y, bins=[nbx, nby], range=((xmin, xmax), (ymin, ymax)), weights=w
         )
 
-        npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_disabled():
+            res0, err0 = func(
+                x,
+                y,
+                bins=[nbx, nby],
+                range=((xmin, xmax), (ymin, ymax)),
+                weights=w,
+                flow=flow,
+            )
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_forced():
+            res0, err0 = func(
+                x,
+                y,
+                bins=[nbx, nby],
+                range=((xmin, xmax), (ymin, ymax)),
+                weights=w,
+                flow=flow,
+            )
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
 
 
 class TestVar2D:
@@ -234,21 +299,26 @@ class TestVar2D:
     @pytest.mark.parametrize("xbins", [E1, E2])
     @pytest.mark.parametrize("ybins", [E1, E2])
     @pytest.mark.parametrize("flow", [False])
-    @pytest.mark.parametrize("ompt", [sys.maxsize, 1])
     @pytest.mark.parametrize("func", [pg.var2d, pg.histogram2d])
     @pytest.mark.TD
     def test_no_weight_and_single_weight(
-        self, xtype, ytype, wtype, xbins, ybins, flow, ompt, func
+        self, xtype, ytype, wtype, xbins, ybins, flow, func
     ):
-        pg.VARIABLE_WIDTH_PARALLEL_THRESHOLD_2D = ompt
         x, y, w = self.make_data(xtype, ytype, wtype)
-        if func == pg.histogram2d:
-            res0, err0 = func(x, y, bins=[xbins, ybins], weights=w, flow=flow)
-        elif func == pg.var2d:
-            res0, err0 = func(x, y, xbins, ybins, weights=w, flow=flow)
         res1, edgex, edgey = np.histogram2d(x, y, bins=[xbins, ybins], weights=w)
 
-        npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_disabled():
+            if func == pg.histogram2d:
+                res0, err0 = func(x, y, bins=[xbins, ybins], weights=w, flow=flow)
+            elif func == pg.var2d:
+                res0, err0 = func(x, y, xbins, ybins, weights=w, flow=flow)
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
+        with pg.omp_forced():
+            if func == pg.histogram2d:
+                res0, err0 = func(x, y, bins=[xbins, ybins], weights=w, flow=flow)
+            elif func == pg.var2d:
+                res0, err0 = func(x, y, xbins, ybins, weights=w, flow=flow)
+            npt.assert_allclose(res0, res1, atol=0.01, rtol=1.0e-3)
 
 
 BAD_TYPES = (np.int8, np.int16, np.uint8, np.uint16, np.float16)
@@ -265,31 +335,28 @@ class TestExceptions:
         x = self.X.astype(xtype)
         w1 = self.W1
         w2 = self.W2
-        ## bad type
-        with pytest.raises(TypeError) as err:
+        # bad type
+        with pytest.raises(TypeError):
             pg.fix1d(x)
 
-        ## bad type
-        with pytest.raises(TypeError) as err:
+        # bad type
+        with pytest.raises(TypeError):
             pg.fix1d(x, weights=w1)
 
-        ## bad shape
-        with pytest.raises(ValueError) as err:
+        # bad shape
+        with pytest.raises(ValueError):
             pg.fix1d(x, weights=w2)
 
-        ## bad type
-        with pytest.raises(TypeError) as err:
+        # bad type
+        with pytest.raises(TypeError):
             pg.fix1dmw(x, weights=w2)
 
     @pytest.mark.parametrize("xtype", BAD_TYPES)
     @pytest.mark.misci
     def test_v1d(self, xtype):
         x = self.X.astype(xtype)
-        w1 = self.W1
-        w2 = self.W2
-
-        ## bad range
-        with pytest.raises(ValueError) as err:
+        # bad range
+        with pytest.raises(ValueError):
             pg.histogram(x.astype(np.float32), bins=[1.0, 2.0, 3.0, 5.0], range=(-1, 1))
 
 
